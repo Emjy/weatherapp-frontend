68c184a304d199fea484eb2341118c8ee32f8443
Emilien <emiliengir@gmail.com>
2024-01-26 16:19:06 +0100

    “xxxx”

---
+++ login.js
@@ -0,0 +1,44 @@
+// Insert your code here
+
+// Enregistrement d'un nouvel user
+document.querySelector('#register').addEventListener('click', () => {
+
+    let name = document.querySelector('#registerName').value;
+    let email = document.querySelector('#registerEmail').value;
+    let password = document.querySelector('#registerPassword').value;
+
+
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, password }),
+    }).then(response => response.json())
+        .then(data => {
+            if (data.result === true) {
+                window.location.assign("index.html")
+
+            }
+        })
+
+})
+
+// Connexion d'un user existant 
+document.querySelector('#connection').addEventListener('click', () => {
+
+    let email = document.querySelector('#registerEmail').value;
+    let password = document.querySelector('#registerPassword').value;
+
+    fetch('http://localhost:3000/users/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+    }).then(response => response.json())
+        .then(data => {
+            if (data.result) {
+                window.location.assign("index.html")
+                return true
+            }
+
+        })
+
+})
